a3c5f692a51e64a620e580c1293d43a021b4ce2d
LeoRiether <leonardoalves8217@gmail.com>
2021-06-09 12:35:50 -0300

    Alert error

--- src.77de5100.js
+++ src.77de5100.js
@@ -20825,7 +20825,7 @@ function submissions_time(status) {
   var maxTime = status.reduce(function (acc, sub) {
     return Math.max(acc, sub.relativeTimeSeconds);
   }, 0);
-  var step = 15 * 60; // 10 minute intervals
+  var step = 15 * 60; // 15 minute intervals
 
   var times = new Array(Math.ceil(maxTime / step)).fill(0).map(function (_, i) {
     return i * step / 60;
@@ -20921,149 +20921,6 @@ var __importStar = this && this.__importStar || function (mod) {
   return result;
 };
 
-var __awaiter = this && this.__awaiter || function (thisArg, _arguments, P, generator) {
-  function adopt(value) {
-    return value instanceof P ? value : new P(function (resolve) {
-      resolve(value);
-    });
-  }
-
-  return new (P || (P = Promise))(function (resolve, reject) {
-    function fulfilled(value) {
-      try {
-        step(generator.next(value));
-      } catch (e) {
-        reject(e);
-      }
-    }
-
-    function rejected(value) {
-      try {
-        step(generator["throw"](value));
-      } catch (e) {
-        reject(e);
-      }
-    }
-
-    function step(result) {
-      result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected);
-    }
-
-    step((generator = generator.apply(thisArg, _arguments || [])).next());
-  });
-};
-
-var __generator = this && this.__generator || function (thisArg, body) {
-  var _ = {
-    label: 0,
-    sent: function sent() {
-      if (t[0] & 1) throw t[1];
-      return t[1];
-    },
-    trys: [],
-    ops: []
-  },
-      f,
-      y,
-      t,
-      g;
-  return g = {
-    next: verb(0),
-    "throw": verb(1),
-    "return": verb(2)
-  }, typeof Symbol === "function" && (g[Symbol.iterator] = function () {
-    return this;
-  }), g;
-
-  function verb(n) {
-    return function (v) {
-      return step([n, v]);
-    };
-  }
-
-  function step(op) {
-    if (f) throw new TypeError("Generator is already executing.");
-
-    while (_) {
-      try {
-        if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-        if (y = 0, t) op = [op[0] & 2, t.value];
-
-        switch (op[0]) {
-          case 0:
-          case 1:
-            t = op;
-            break;
-
-          case 4:
-            _.label++;
-            return {
-              value: op[1],
-              done: false
-            };
-
-          case 5:
-            _.label++;
-            y = op[1];
-            op = [0];
-            continue;
-
-          case 7:
-            op = _.ops.pop();
-
-            _.trys.pop();
-
-            continue;
-
-          default:
-            if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) {
-              _ = 0;
-              continue;
-            }
-
-            if (op[0] === 3 && (!t || op[1] > t[0] && op[1] < t[3])) {
-              _.label = op[1];
-              break;
-            }
-
-            if (op[0] === 6 && _.label < t[1]) {
-              _.label = t[1];
-              t = op;
-              break;
-            }
-
-            if (t && _.label < t[2]) {
-              _.label = t[2];
-
-              _.ops.push(op);
-
-              break;
-            }
-
-            if (t[2]) _.ops.pop();
-
-            _.trys.pop();
-
-            continue;
-        }
-
-        op = body.call(thisArg, _);
-      } catch (e) {
-        op = [6, e];
-        y = 0;
-      } finally {
-        f = t = 0;
-      }
-    }
-
-    if (op[0] & 5) throw op[1];
-    return {
-      value: op[0] ? op[1] : void 0,
-      done: true
-    };
-  }
-};
-
 var __importDefault = this && this.__importDefault || function (mod) {
   return mod && mod.__esModule ? mod : {
     "default": mod
@@ -21111,50 +20968,41 @@ var $ = {
   }
 };
 
-function main(contestID) {
-  return __awaiter(this, void 0, void 0, function () {
-    var creds, status, verdicts_canvas, verdicts_per_problem_canvas, submissions_time_canvas, acs_time_canvas;
-    return __generator(this, function (_a) {
-      switch (_a.label) {
-        case 0:
-          creds = {
-            key: localStorage.getItem('cf_key'),
-            secret: localStorage.getItem('cf_secret')
-          };
-          return [4
-          /*yield*/
-          , api.contest_status(creds, contestID)];
+function make_all_charts(status) {
+  var verdicts_canvas = make_canvas({
+    'id': 'verdicts-canvas',
+    'style': 'height: 360px;'
+  }, $.q('#verdicts'));
+  new auto_1.default(verdicts_canvas, verdicts_1.default(status));
+  var verdicts_per_problem_canvas = make_canvas({
+    'id': 'verdicts-per-problem-canvas',
+    'style': 'height: 600px;'
+  }, $.q('#verdicts-per-problem'));
+  new auto_1.default(verdicts_per_problem_canvas, verdicts_per_problem_1.default(status));
+  var submissions_time_canvas = make_canvas({
+    'id': 'submissions-time-canvas',
+    'style': 'height: 600px;'
+  }, $.q('#submissions-time'));
+  new auto_1.default(submissions_time_canvas, submissions_time_1.default(status));
+  var acs_time_canvas = make_canvas({
+    'id': 'acs-time-canvas',
+    'style': 'height: 600px;'
+  }, $.q('#acs-time'));
+  new auto_1.default(acs_time_canvas, submissions_time_1.default(status.filter(function (sub) {
+    return sub.verdict == "OK";
+  })));
+}
 
-        case 1:
-          status = _a.sent();
-          status = status.filter(is_submission_from_contestant);
-          verdicts_canvas = make_canvas({
-            'id': 'verdicts-canvas',
-            'style': 'height: 360px;'
-          }, $.q('#verdicts'));
-          new auto_1.default(verdicts_canvas, verdicts_1.default(status));
-          verdicts_per_problem_canvas = make_canvas({
-            'id': 'verdicts-per-problem-canvas',
-            'style': 'height: 600px;'
-          }, $.q('#verdicts-per-problem'));
-          new auto_1.default(verdicts_per_problem_canvas, verdicts_per_problem_1.default(status));
-          submissions_time_canvas = make_canvas({
-            'id': 'submissions-time-canvas',
-            'style': 'height: 600px;'
-          }, $.q('#submissions-time'));
-          new auto_1.default(submissions_time_canvas, submissions_time_1.default(status));
-          acs_time_canvas = make_canvas({
-            'id': 'acs-time-canvas',
-            'style': 'height: 600px;'
-          }, $.q('#acs-time'));
-          new auto_1.default(acs_time_canvas, submissions_time_1.default(status.filter(function (sub) {
-            return sub.verdict == "OK";
-          })));
-          return [2
-          /*return*/
-          ];
-      }
-    });
+function main(contestID) {
+  // Yeah, an extension or userscript could easily get hold of these
+  var creds = {
+    key: localStorage.getItem('cf_key'),
+    secret: localStorage.getItem('cf_secret')
+  };
+  return api.contest_status(creds, contestID).then(function (status) {
+    return status.filter(is_submission_from_contestant);
+  }).then(make_all_charts).catch(function (err) {
+    return alert(err);
   });
 }
 
@@ -21164,7 +21012,6 @@ function main(contestID) {
     var secret = $.id('secret-input').value;
     localStorage.setItem('cf_key', key);
     localStorage.setItem('cf_secret', secret);
-    window.location.reload();
   });
   $.id('contest-id').value = localStorage.getItem('last_contest_id') || "";
   $.id('vis-btn').addEventListener('click', function () {
@@ -21205,7 +21052,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52167" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56672" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};

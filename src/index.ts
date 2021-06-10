92550a48a7d1203b00e120726d298ad2cf24db18
LeoRiether <leonardoalves8217@gmail.com>
2021-06-10 20:16:47 -0300

    Forced dark theme

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as api from './api';
 import { Submission } from './api_interfaces';
-import Chart from 'chart.js/auto';
+import Chart  from 'chart.js/auto';
+import * as chart from 'chart.js';
 
 import verdicts from './charts/verdicts';
 import verdicts_per_problem from './charts/verdicts_per_problem';
@@ -22,6 +23,14 @@ function make_canvas(attrs: object, parent?: Element): HTMLCanvasElement {
     return canvas;
 }
 
+function make_chart(data: chart.ChartConfiguration,
+                    canvas_attr: object,
+                    parent_element: Element) {
+
+    let canvas = make_canvas(canvas_attr, parent_element);
+    new Chart(canvas, data);
+}
+
 const $ = {
     id(id: string) { return document.getElementById(id); },
     q(query: string) { return document.querySelector(query); },
@@ -29,35 +38,35 @@ const $ = {
 };
 
 function make_all_charts(status: Submission[]) {
-    let verdicts_canvas = make_canvas(
+    make_chart(
+        verdicts(status),
         { 'id': 'verdicts-canvas', 'style': 'height: 360px;' },
-        $.q('#verdicts'));
+        $.q('#verdicts'),
+    );
 
-    new Chart(verdicts_canvas, verdicts(status));
-
-    let verdicts_per_problem_canvas = make_canvas(
+    make_chart(
+        verdicts_per_problem(status),
         { 'id': 'verdicts-per-problem-canvas', 'style': 'height: 550px;' },
-        $.q('#verdicts-per-problem'));
+        $.q('#verdicts-per-problem'),
+    );
 
-    new Chart(verdicts_per_problem_canvas, verdicts_per_problem(status));
-
-    let submissions_time_canvas = make_canvas(
+    make_chart(
+        submissions_time(status),
         { 'id': 'submissions-time-canvas', 'style': 'height: 600px;' },
-        $.q('#submissions-time'));
+        $.q('#submissions-time'),
+    );
 
-    new Chart(submissions_time_canvas, submissions_time(status));
-
-    let acs_time_canvas = make_canvas(
+    make_chart(
+        submissions_time(status.filter(sub => sub.verdict == "OK")),
         { 'id': 'acs-time-canvas', 'style': 'height: 600px;' },
-        $.q('#acs-time'));
+        $.q('#acs-time'),
+    );
 
-    new Chart(acs_time_canvas, submissions_time(status.filter(sub => sub.verdict == "OK")));
-
-    let submissions_by_lang = make_canvas(
+    make_chart(
+        submissions_lang(status),
         { 'id': 'subs-by-lang-canvas', 'style': 'height: 300px;' },
-        $.q('#subs-by-lang'));
-
-    new Chart(submissions_by_lang, submissions_lang(status));
+        $.q('#subs-by-lang'),
+    );
 }
 
 function main(contestID: string) {
